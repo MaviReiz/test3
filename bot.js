245363976d0c902fc7eac58ef65976baa8866e2a
MaviReiz <44947741+MaviReiz@users.noreply.github.com>
2018-11-24 11:05:04 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,7 +1,7 @@
 const Discord = require("discord.js") 
 const settings = require("./your_settings.json")
 const bot = new Discord.Client()
-bot.user.setGame('By MaviReiz')
+bot.user.setGame('By MaviReiz')()
 bot.on('ready', async => {
 console.log("Rainbow bot is ready!" + "\n" + bot.user.tag + "\n" + "Server Count: "  + bot.guilds.size + "\n" + "Cached users: " + bot.users.size + "\n" + "Enjoy!")
 });
