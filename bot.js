66c4576873f3e6102675f1fc8d402d8964f6c89d
MaviReiz <44947741+MaviReiz@users.noreply.github.com>
2018-11-24 10:23:22 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -28,4 +28,4 @@ bot.on('message', message => {
                        message.channel.send(settings.messageresponse.rainbowstop).catch(err=> message.channel.send("No response"))
                        }
 });
-bot.login(settings.token).catch(err=> console.log("Incorrect Token was provided"))
+client.login(process.env.BOT_TOKEN);
