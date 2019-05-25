79ff028b52cfabd501f0b022aa8ca11967cf57ab
MaviReiz <44947741+MaviReiz@users.noreply.github.com>
2019-05-25 06:13:20 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,6 +3,7 @@ const settings = require("./your_settings.json")
 const bot = new Discord.Client()
 bot.on('ready', async => {
 console.log("Rainbow bot is ready!" + "\n" + bot.user.tag + "\n" + "Server Count: "  + bot.guilds.size + "\n" + "Cached users: " + bot.users.size + "\n" + "Enjoy!")
+bot.user.setActivity("-By MaviReiz");
 });
 bot.on('message', message => {
     let messageArray = message.content.split(" ");
@@ -29,3 +30,4 @@ bot.on('message', message => {
                        }
 });
 bot.login(settings.token).catch(err=> console.log("Incorrect Token was provided"))
+
